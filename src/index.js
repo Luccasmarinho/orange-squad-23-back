c61b5863b5e5cb58e924f17afaad8cf4aeca0ec7
JuanLucca846 <lucca470@gmail.com>
2024-01-23 14:38:37 -0300

    feat: create project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require("express");
+const cors = require("cors");
+const projectRouter = require("./router/projects.routes");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(projectRouter);
 
 const port = process.env.PORT || 3001;
 
