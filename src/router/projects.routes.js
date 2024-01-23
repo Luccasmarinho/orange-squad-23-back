b6d9744c5ecb7238ebe3dd02b8ff8212677fe808
JuanLucca846 <lucca470@gmail.com>
2024-01-23 19:43:33 -0300

    feat: list project

--- src/router/projects.routes.js
+++ src/router/projects.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const projectRouter = express.Router();
 const createProjectController = require("../controllers/project/create-project-controller");
+const listAllProjectsController = require("../controllers/project/list-project-controller");
 
-projectRouter.post("/projects", createProjectController);
+projectRouter.post("/project", createProjectController);
+projectRouter.get("/projects", listAllProjectsController);
 
 module.exports = projectRouter;
---
+++ src/services/project/list-project-service.js
@@ -0,0 +1,17 @@
+const prismaClient = require("../../prisma/prisma-client");
+
+const listAllProjectsService = async () => {
+  const allProjects = await prismaClient.projects.findMany({
+    select: {
+      id: true,
+      title: true,
+      tags: true,
+      link: true,
+      description: true,
+      projectCover: true,
+    },
+  });
+  return allProjects;
+};
+
+module.exports = listAllProjectsService;
