42c164fa12d768b594c8a1161abc836722b613d0
Luccasmarinho <luccas88oliveira@gmail.com>
2024-01-23 00:23:10 -0300

    feat: creating folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(cors());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
